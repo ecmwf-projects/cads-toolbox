12b433ca7a5aacb1c140ececdfc35ebd50845014
Alessandro Amici <a.amici@bopen.eu>
2022-08-26 12:28:52 +0200

    tentative

--- cads_toolbox/catalogue.py
+++ cads_toolbox/catalogue.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import functools
 from typing import Any, Dict, Optional
 
 import cdsapi
@@ -24,12 +25,29 @@ def collection(collection_id: str) -> Dict[str, Any]:
 
 
 class Remote:
-    def __init__(self, collection_id: str, request: Dict[str, Any]) -> None:
+    def __init__(
+        self, collection_id: str, request: Dict[str, Any], cache: bool = True
+    ) -> None:
         self.client = cdsapi.Client()
         self.result = self.client.retrieve(collection_id, request)
+        self.cache_path = None
 
-    def download(self, target: Optional[str] = None) -> None:
-        self.result.download(target)
+    def download(self, target: Optional[str] = None, cache: bool = True) -> None:
+        # FIXE: manage cache == False
+        self.download_to_cache()
+        # copy(self.cache_path, target)
+
+    def download_to_cache(self) -> None:
+        self.result.download(self.cache_path)
+
+    def to_xarray(self, *args, **kwargs):
+        return self.teal.to_xarray(*args, **kwargs)
+
+    @functools.cached_property
+    def teal(self):
+        import teal
+
+        return teal.open(self.cache_path)
 
 
 def retrieve(collection_id: str, request: Dict[str, Any]) -> Remote:
