29ade84e427eac83d7c5d42139f773939232b469
Eddy <Edward.Comyn-Platt@ecmwf.int>
2022-12-06 09:42:01 +0000

    stash commit

--- cads_toolbox/analysis/__init__.py
+++ cads_toolbox/analysis/__init__.py
@@ -1,31 +1,8 @@
-import numpy
-import emohawk
 import coucal
 
-#@emhawk_to_format('numpy')
-def nanmean(data, *args, **kwargs):
-    data = emohawk.open(data).to_numpy()
-    return numpy.nanmean(*args, **kwargs)
-
-
-@emohawk_to_format('xarray')
-def climatology_mean(dataarray, *args, **kwargs):
-    return coucal.climatology_mean(dataarray, *args, **kwargs)
-
-
-@emohawk_to_format('xarray', 'geopandas')
-def shape_average(dataarray, shape, *args, **kwargs):
-    return coucal.shapes.average(dataarray, shape, *args, **kwargs)
+from cads_toolbox.cadsify import cadsify_function
 
 
 shape_average = cadsify_function(
     coucal.shapes.average,
 )
-
-
-
-@cadsify_function(coucal.shapes.average)
-def shape_average(dataarray, shape, *args, **kwargs):
-    dataarray = emohawk(dataarray).to_xarray()
-    shape = emohawk(shape).to_geopandas()
-    return coucal.shapes.average(dataarray, shape, *args, **kwargs)
--- cads_toolbox/cadsify.py
+++ cads_toolbox/cadsify.py
@@ -1,9 +1,16 @@
+from functools import wraps
 
-def cadsify(function):
+def cadsify_function(function):
     # Wrapper to ensure inputs to function are appropriate for the
     # function.
     # Tasks:
     #  ensure docstrings are imported
     #  data objects correct format
     #  args+kwargs correctly parsed.
+
+    @wraps(function)
+    def _function(*args, **kwargs):
+        function(*args, **kwargs)
+
+    return _function
 
