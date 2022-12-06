ced7106670504ccc1a8dc34920c0c242d3137699
Eddy <Edward.Comyn-Platt@ecmwf.int>
2022-12-06 09:03:09 +0000

    stash commit

---
+++ cads_toolbox/cadsify.py
@@ -0,0 +1,9 @@
+
+def cadsify(function):
+    # Wrapper to ensure inputs to function are appropriate for the
+    # function.
+    # Tasks:
+    #  ensure docstrings are imported
+    #  data objects correct format
+    #  args+kwargs correctly parsed.
+
